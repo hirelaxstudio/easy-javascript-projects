56e06d6973bbaba4e01f5c722c819b63531ad7e4
Eyüp Kağan Eyüpoğlu <72672133+hirelaxstudio@users.noreply.github.com>
2023-11-08 20:24:56 +0300

    2

--- Mouseover Element/script.js
+++ Mouseover Element/script.js
@@ -1,15 +1,98 @@
-// HTML'deki bütün .ball elementlerini seçin
-const balls = document.querySelectorAll('.ball');
+console.log("script.js is running");
 
-// Her bir top için bir olay dinleyici ekleyin
-balls.forEach(ball => {
-    ball.addEventListener('mouseover', () => {
-        // Topu büyütmek için CSS transform özelliğini kullanın
-        ball.style.transform = 'scale(1.2)';
-    });
+var canvas = document.getElementById("canvas");
+var c = canvas.getContext("2d");
+var tx = window.innerWidth;
+var ty = window.innerHeight;
+canvas.width = tx;
+canvas.height = ty;
 
-    ball.addEventListener('mouseout', () => {
-        // Fareyi topun üzerinden çektiğinizde normal boyuta döndürün
-        ball.style.transform = 'scale(1)';
-    });
+var mousex = 0;
+var mousey = 0;
+
+canvas.addEventListener("mousemove", function(event) {
+  mousex = event.clientX;
+  mousey = event.clientY;
 });
+
+var grav = 0.99;
+
+function randomColor() {
+  return (
+    "rgba(" +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.round(Math.random() * 250) +
+    "," +
+    (Math.random() + 0.1).toFixed(1) +
+    ")"
+  );
+}
+
+function Ball() {
+  this.color = randomColor();
+  this.radius = Math.random() * 20 + 14;
+  this.startradius = this.radius;
+  this.x = Math.random() * (tx - this.radius * 2) + this.radius;
+  this.y = Math.random() * (ty - this.radius);
+  this.dy = Math.random() * 2;
+  this.dx = Math.round((Math.random() - 0.5) * 10);
+  this.vel = Math.random() / 5;
+  this.update = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    c.fillStyle = this.color;
+    c.fill();
+  };
+}
+
+var bal = [];
+for (var i = 0; i < 50; i++) {
+  bal.push(new Ball());
+}
+
+function animate() {
+  if (tx != window.innerWidth || ty != window.innerHeight) {
+    tx = window.innerWidth;
+    ty = window.innerHeight;
+    canvas.width = tx;
+    canvas.height = ty;
+  }
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, tx, ty);
+  for (var i = 0; i < bal.length; i++) {
+    bal[i].update();
+    bal[i].y += bal[i].dy;
+    bal[i].x += bal[i].dx;
+    if (bal[i].y + bal[i].radius >= ty) {
+      bal[i].dy = -bal[i].dy * grav;
+    } else {
+      bal[i].dy += bal[i].vel;
+    }
+    if (bal[i].x + bal[i].radius > tx || bal[i].x - bal[i].radius < 0) {
+      bal[i].dx = -bal[i].dx;
+    }
+    if (
+      mousex > bal[i].x - 20 &&
+      mousex < bal[i].x + 20 &&
+      mousey > bal[i].y - 50 &&
+      mousey < bal[i].y + 50 &&
+      bal[i].radius < 70
+    ) {
+      bal[i].radius += 5;
+    } else {
+      if (bal[i].radius > bal[i].startradius) {
+        bal[i].radius -= 5;
+      }
+    }
+  }
+}
+
+animate();
+
+setInterval(function() {
+  bal.push(new Ball());
+  bal.splice(0, 1);
+}, 400);
